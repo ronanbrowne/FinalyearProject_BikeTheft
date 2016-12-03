f71820b7bae27284d117c6ea3ecf82de2d248d53
Ronan Browne <ronanbrowne88@gmail.com>
2016-12-03 11:55:16 +0000

    heat map / map buttons / specific market details

--- app/src/main/java/com/example/ronan/practicenavigationdrawer/GmapFragment.java
+++ app/src/main/java/com/example/ronan/practicenavigationdrawer/GmapFragment.java
@@ -4,8 +4,10 @@ import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -28,6 +30,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
@@ -50,17 +53,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.example.ronan.practicenavigationdrawer.R.drawable.user;
+import static com.google.android.gms.internal.zzsr.My;
 
 
 public class GmapFragment extends Fragment implements OnMapReadyCallback {
 
-    public MainActivity mainActivity;
 
 
     private GoogleMap gMap;
+    TileOverlay mOverlay;
 
     // Declare a variable for the cluster manager.
-    private ClusterManager<MyItemMapClusters> mClusterManager;
+   // private ClusterManager<MyItemMapClusters> mClusterManager;
 
     private View myView;
     ImageView image;
@@ -72,8 +76,6 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
     private DatabaseReference stolenBikesDatabse;
     BikeData mybike = new BikeData();
 
-    ArrayList<Double> latitude = new ArrayList<>();
-    ArrayList<Double> longditude = new ArrayList<>();
     List<LatLng> coOr = new ArrayList<>();
 
 
@@ -95,23 +97,37 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
                 LatLng coOrdinates = new LatLng(mybike.getLatitude(), mybike.getLongditude());
                 coOr.add(coOrdinates);
 
-                            //  clusters
+                //  **CODE RELATED TO CLUSTERS . COMPLEX TO IMPLEMEND TITLE SNIPPET, RETURN TO AT LATER STAGE **
 
 //                MyItemMapClusters offsetItem = new MyItemMapClusters(mybike.getLatitude(), mybike.getLongditude());
 //                mClusterManager.addItem(offsetItem);
+//                gMap.setOnCameraIdleListener(mClusterManager);
+//                gMap.setOnMarkerClickListener(mClusterManager);
+
 
 
                 //move Camera and add marker to coOrdinates position
                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coOrdinates, 6));
-//hidden for now
 
-              gMap.addMarker(new MarkerOptions().title("Make: " + mybike.getMake()).snippet("Model:" + mybike.getModel() + "\nLast seen: ").position(coOrdinates));
 
-               //HEATMAP
+
+                  //  if (mybike.getImageBase64().equals("No image") || mybike.getImageBase64() == null){
+                        gMap.addMarker(new MarkerOptions().title("Make: " + mybike.getMake()).snippet("Model:" + mybike.getModel() + "\nLast seen: " + mybike.getColor()).position(coOrdinates));
+
+//                    }else{
+//                        int height = 100;
+//                        int width = 100;
+//                        Bitmap smallMarker = Bitmap.createScaledBitmap(getBitMapFromString(mybike.getImageBase64()), width, height, false);
+//
+//                        gMap.addMarker(new MarkerOptions().title("Make: " + mybike.getMake()).snippet("Model:" + mybike.getModel() + "\nLast seen: " + mybike.getColor()).position(coOrdinates).icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+//
+//                    }
+
+
+
+                //HEATMAP
                 // Create a heat map tile provider, passing it the latlngs of the police stations.
-
-
-               // gMap.addTileOverlay(mOverlay);
+                // gMap.addTileOverlay(mOverlay);
 
                 //http://stackoverflow.com/questions/13904651/android-google-maps-v2-how-to-add-marker-with-multiline-snippet
 
@@ -136,16 +152,6 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
                         TextView snippet = new TextView(getActivity().getApplicationContext());
                         snippet.setTextColor(Color.GRAY);
                         snippet.setText(marker.getSnippet());
-
-
-                        if (getBitMapFromString(mybike.getImageBase64()) != null) {
-
-                            image = new ImageView(getActivity().getApplicationContext());
-                            image.setImageBitmap(getBitMapFromString(mybike.getImageBase64()));
-                            image.getLayoutParams().height = 50;
-                            image.getLayoutParams().width = 50;
-                            image.requestLayout();
-                        }
 
 
                         info.addView(title);
@@ -178,8 +184,6 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
 
         final View rootView = inflater.inflate(R.layout.fragment_map, container, false);
         myView = rootView;
-
-        mainActivity = (MainActivity) getActivity();
 
         //  firebase
         stolenBikesDatabse = FirebaseDatabase.getInstance().getReference().child("Stolen Bikes");
@@ -217,7 +221,6 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
         });
 
 
-
         heatMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -241,12 +244,6 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
     }
 
 
-
-
-
-
-
-
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -262,13 +259,15 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
         googleMap.clear();
 
         this.gMap = googleMap;
+        LatLng dub = new LatLng(53.3498 ,-6.2603);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dub, 6));
 
-        mClusterManager = new ClusterManager<MyItemMapClusters>(getActivity().getApplicationContext(), gMap);
+     //   mClusterManager = new ClusterManager<MyItemMapClusters>(getActivity().getApplicationContext(), gMap);
 
         // Point the map's listeners at the listeners implemented by the cluster
         // manager.
-        gMap.setOnCameraIdleListener(mClusterManager);
-        gMap.setOnMarkerClickListener(mClusterManager);
+      //  gMap.setOnCameraIdleListener(mClusterManager);
+       // gMap.setOnMarkerClickListener(mClusterManager);
 
 
         stolenBikesDatabse.addValueEventListener(bikeListener);
@@ -293,48 +292,18 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
         return bitmap;
     }
 
-
-    private void setUpClusterer() {
-        // Position the map.
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
-
-        // Initialize the manager with the context and the map.
-        // (Activity extends context, so we can pass 'this' in the constructor.)
-        //  mClusterManager = new ClusterManager<MyItemMapClusters>(getActivity().getApplicationContext(), gMap);
-
-        // Point the map's listeners at the listeners implemented by the cluster
-        // manager.
-        gMap.setOnCameraIdleListener(mClusterManager);
-        gMap.setOnMarkerClickListener(mClusterManager);
-
-        // Add cluster items (markers) to the cluster manager.
-        // addItems();
-    }
-
-
-
-    HeatmapTileProvider mProvider;
-    TileOverlay mOverlay;
     private void addHeatMap() {
-     //   List<LatLng> list;
-
-
         gMap.clear();
-
         // Get the data: latitude/longitude positions of police stations.
         HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder()
                 .data(coOr).radius(35)
                 .build();
         // Add a tile overlay to the map, using the heat map tile provider.
-         mOverlay = gMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
-
+        mOverlay = gMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
         mOverlay.setVisible(true);
-
-
-
     }
 
-    public void removeHeatMap(){
+    public void removeHeatMap() {
         mOverlay.remove();
     }
 
