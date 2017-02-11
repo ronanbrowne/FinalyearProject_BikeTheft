529cd726fff60688129b5b7b4c741033ea36f265
Ronan Browne <ronanbrowne88@gmail.com>
2017-02-11 16:47:54 +0000

    startign work on xml for sensor search

---
+++ app/src/main/java/com/example/ronan/bikepro/Scan_For_Stolen.java
@@ -0,0 +1,29 @@
+package com.example.ronan.bikepro;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Scan_For_Stolen extends Fragment {
+
+
+    public Scan_For_Stolen() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scan__for__stolen, container, false);
+    }
+
+}
